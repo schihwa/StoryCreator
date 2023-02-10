ed52ffc2bac1c6c7fc5572b56da15dad6610f21f
schihwa <schihwa109@gmail.com>
2023-02-10 03:18:03 +0000

    removed redundant imports cleaned redundant code adding meaningful comments made formatting more consistent

--- StoryCreator.py
+++ StoryCreator.py
@@ -4,37 +4,30 @@ import re
 from pydub import AudioSegment, silence
 import pyttsx3
 import pysubs2
-import soundfile
-from moviepy.editor import *
-
 
 def videoFormatter():
+    #you can copy and paste your story here. hyphens ruin the timings of subtitles
+    speech = """ your story """
 
     # deines required variables
-
-
-
-
-    speech = """our hostess left to take care of the dessert preparations. As I looked at the open window and a cartoon light bulb appeared over my head. I knew I had to be decisive. I committed. I grabbed the steak and executed a perfect throw at the open window. The window wasn't open. It was the cleanest window you've ever seen in your life. That was before the slab of steak was slowly sliding down leaving a trail of red juice. My wife turned, jaw dropped, and stared at me like I was an alien from another planet. This look then slowly turned into more of a. There is no place on this planet you can ever hide look of demonic anger. The hostess heard the thud and quickly came over. She took in the scene, the steak sitting on the windowsill and my empty plate. All I could say was sorry. I was cutting it and it slipped. Right honey? Both women just stared at me like I escaped the loony bin"""
-    
-
     soundFile = "speech.wav"
     subtitlesFile = "script.ssa"
     videoFile = "test.mp4"
     backgroundSound = "test.mp3"
     video = "StoryTime.mp4"
-    # creates audio file of the speech
 
+    # creates audio file of the speech
     textToSpeech(speech, soundFile)
+
     # creates SSA file of the subtitlies
     subtitles(speech, soundFile, subtitlesFile)
-    
 
-    soundFileDuration = soundfile.SoundFile('speech.wav').frames / soundfile.SoundFile('speech.wav').samplerate
+    #cuts video appropriate for youtube shorts
+    soundFileDuration = soundFile.SoundFile('speech.wav').frames / soundFile.SoundFile('speech.wav').samplerate
     if soundFileDuration > 60:
         soundFileDuration = 59
 
-        
+    #mute the original sound of the mp4 file and add the sound of the selected music and the Text to speech sound file
     command = [
             "ffmpeg",
             "-i", videoFile,
@@ -48,37 +41,38 @@ def videoFormatter():
             "-crf", "1",
             video
         ]
-    
+
     subprocess.run(command)
 
 
 def textToSpeech(speech, soundFile):
-    # Initialize the TTS engine
+    #Initialize the TTS engine
     engine = pyttsx3.init()
 
-    # Set the voice to use
+    #Set the voice to use
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
 
-    # Set the rate of speech
+    #Set the rate of speech
     engine.setProperty('rate', 170)
 
-    # Set the volume
+    #Set the volume
     engine.setProperty('volume', 1.0)
 
-    # Set the pauses
+    #Set the pauses
     engine.setProperty('pause', 0.3)
 
-    # Save the story to speech
+    #Save the story to speech
     engine.save_to_file(speech, soundFile)
     engine.runAndWait()
 
+
 def subtitles(speech, soundFile, subtitlesFile):
-    # Split the story into sentences
+    #Split the story into sentences
     subtitles = re.split(r'[,.;:!?]', speech)
     subtitles = [sub.strip() for sub in subtitles if sub and not sub.isspace()]
 
-    # Create the subtitles using pysrt
+    #Create the subtitles using pysrt
     subs = pysubs2.SSAFile()
 
     myaudio = AudioSegment.from_mp3(soundFile)
@@ -86,10 +80,10 @@ def subtitles(speech, soundFile, subtitlesFile):
     silences = silence.detect_silence(
         myaudio, min_silence_len=40, silence_thresh=dBFS-1600)
 
-    # put time stamps in silences
+    #put time stamps in silences
     silences = [((start), (stop)) for start, stop in silences]
 
-    # Create a new list with time stamps between pauses
+    #Create a new list with time stamps between pauses
     time_stamps = []
     previous_end = silences[0][0]
 
